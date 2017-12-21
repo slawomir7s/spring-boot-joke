96ed0bc78c6517d96234e1eb1bca8cd2d7eb77a2
ssasza <slawomir_s@tlen.pl>
2017-12-21 09:55:30 +0100

    First app version

---
+++ src/main/java/sasza/springbootjoke/SpringBootJokeApplication.java
@@ -0,0 +1,12 @@
+package sasza.springbootjoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJokeApplication.class, args);
+	}
+}
